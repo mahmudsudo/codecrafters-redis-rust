a5c4499d945c2b991a723af49df794a6677d8674
mahmudsudo <blinkztyler@gmail.com>
2022-12-25 22:58:33 +0100

    pass 3nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{net::TcpListener, io::{Read, Write}};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -7,9 +10,16 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 let mut buf = [0; 512];
-                stream.read(&mut buf).unwrap();
-                stream.write("+PONG\r\n".as_bytes()).unwrap();
-               
+                loop {
+                    // Wait for the client to send us a message but ignore the content for now
+                    let bytes_read = stream.read(&mut buf).unwrap();
+                    if bytes_read == 0 {
+                        println!("client closed the connection");
+                        break;
+                    }
+
+                    stream.write("+PONG\r\n".as_bytes()).unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
